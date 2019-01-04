d454a394d656fbcb9269755a691647a1234009f7
Albert Kuliński <albert.kulinski@hotmail.com>
2019-01-04 15:22:32 +0100

    initial commit

---
+++ src/main/java/com/akulinski/notesservice/NotesserviceApplication.java
@@ -0,0 +1,14 @@
+package com.akulinski.notesservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotesserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotesserviceApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/com/akulinski/notesservice/NotesserviceApplicationTests.java
@@ -0,0 +1,17 @@
+package com.akulinski.notesservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NotesserviceApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
